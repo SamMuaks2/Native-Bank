004a2023022e3d3adb004721bbe8bdd6a2c0b060
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-15 16:26:17 +0100

    Add more files

---
+++ Native-Bank/constants/icons.tsx
@@ -0,0 +1,25 @@
+import DollarIcon from "@/assets/icons/dollar-sign-solid.svg";
+import WalletAddMoneyIcon from "@/assets/icons/wallet-solid.svg";
+import WalletCardIcon from "@/assets/icons/ewallet.png";
+import AmazonIcon from "@/assets/icons/amazon-pay-brands-solid.svg";
+import UberIcon from "@/assets/icons/uber-brands-solid.svg";
+import AirbnbIcon from "@/assets/icons/airbnb-brands-solid.svg";
+import DollarSignIcon from "@/assets/icons/sack-dollar-solid.svg";
+import FigmaIcon from "@/assets/icons/figma-brands-solid.svg";
+import ShopingCartIcon from "@/assets/icons/cart-shopping-solid.svg";
+import SpotifyIcon from "@/assets/icons/spotify-brands-solid.svg";
+import NetflixIcon from "@/assets/icons/netflix.png";
+
+export {
+    DollarIcon,
+    WalletAddMoneyIcon,
+    WalletCardIcon,
+    AmazonIcon,
+    UberIcon,
+    AirbnbIcon,
+    DollarSignIcon,
+    FigmaIcon,
+    ShopingCartIcon,
+    SpotifyIcon,
+    NetflixIcon,
+}
