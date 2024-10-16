bb83e09f1746fcf50dfafe5b47e55dbfb2514f93
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-16 14:38:47 +0100

    Fix error

---
+++ Native-Bank/app/(tabs)/transactions.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Transactions = () => {
+  return (
+    <View>
+      <Text>Transactions is in building state</Text>
+    </View>
+  );
+};
+
+export default Transactions;
