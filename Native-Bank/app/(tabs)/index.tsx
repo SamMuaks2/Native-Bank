ffc58edfeb9e7a83d1ab79260d5984e5a6c958c9
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-16 16:32:09 +0100

    Start header

--- Native-Bank/app/(tabs)/index.tsx
+++ Native-Bank/app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Colors from "@/constants/Colors";
+import { Stack } from "expo-router";
+import Header from "@/components/Header";
 
 const Page = () => {
   return (
+    <>
+    <Stack.Screen options={{
+        header: () => <Header />
+    }} />
     <View style={styles.container}>
       <Text>Page is in building state</Text>
     </View>
+    </>
   );
 };
 
---
+++ Native-Bank/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+
+const Header = () => {
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: Colors.black}}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', height: 70, alignItems: 'center', paddingHorizontal: 20,}}>
+            <Image source={{uri: 'https://i.pravatar.cc/250?u=12'}} style={{height: 50, width: 50, borderRadius: 30,}} />
+            <TouchableOpacity onPress={() => {}} style={{borderColor: '#666', borderWidth: 1, padding: 8, borderRadius: 10,}}>
+                <Text style={{color: Colors.white, fontSize: 12,}}>My Transactions</Text>
+            </TouchableOpacity>
+        </View>
+        </SafeAreaView>
+    )
+}
+
+export default Header
+
+const styles = StyleSheet.create({})
