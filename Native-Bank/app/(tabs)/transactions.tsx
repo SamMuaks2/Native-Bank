f60ace74849897660f849c0a1360c8250bf6302a
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-16 15:42:21 +0100

    Add page styles

--- Native-Bank/app/(tabs)/transactions.tsx
+++ Native-Bank/app/(tabs)/transactions.tsx
@@ -1,12 +1,29 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Colors from "@/constants/Colors";
+import { Stack } from "expo-router";
 
 const Transactions = () => {
   return (
-    <View>
-      <Text>Transactions is in building state</Text>
-    </View>
+    <>
+        <Stack.Screen options={{headerShown: false}} />
+        <View style={styles.container}>
+        <Text style={styles.text}>Transactions is in building state</Text>
+        </View>
+    </>
   );
 };
 
 export default Transactions;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.black
+    },
+    text: {
+        color: Colors.white
+    }
+})
