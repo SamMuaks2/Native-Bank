bc99015b3ae20c2ab472cfc46621ddd53d7abe8f
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-18 12:20:27 +0100

    Add income amount

--- Native-Bank/components/IncomeBlock.tsx
+++ Native-Bank/components/IncomeBlock.tsx
@@ -1,20 +1,35 @@
-import { FlatList, ListRenderItem, StyleSheet, Text, View } from "react-native";
+import { FlatList, ListRenderItem, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import Colors from "@/constants/Colors";
 import { IncomeType } from "@/types";
+import { DollarIcon } from "@/constants/icons";
+import { Feather } from "@expo/vector-icons";
+import amount from "../data/income.json";
 
 const IncomeBlock = ({incomeList} : {incomeList: IncomeType[]}) => {
-    const renderItem:ListRenderItem<IncomeType> = ({item}) => {
+    const renderItem:ListRenderItem<IncomeType> = ({item, index}) => {
+        let amount = item.amount.split('.');
         return(
-            <View>
+            <View style={{backgroundColor: Colors.grey, padding: 20, borderRadius: 20, marginRight: 15, width: 150, gap: 10,}}>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <View style= {{borderColor: '#666', borderWidth: 1, borderRadius: 50, padding: 5, alignSelf: 'flex-start',}}>
+                <DollarIcon width={22} height={22} color={Colors.white} />
+                </View>
+
+                <TouchableOpacity onPress={() => {}}>
+                    <Feather name="more-horizontal" size={20} color={Colors.white} />
+                </TouchableOpacity>
+                </View>
                 <Text style={{color: Colors.white}}>{item.name}</Text>
+                <Text style={{color: Colors.white, fontSize: 18, fontWeight: "600",}}>${item.amount [0]}.
+                    <Text style={{fontSize:14, fontWeight: '400'}}>{item.amount [1]}</Text></Text>
             </View>
         );
     }
 
     return (
         <View>
-            <Text style={{color: Colors.white, fontSize: 16,}}>
+            <Text style={{color: Colors.white, fontSize: 16, marginBottom: 20,}}>
                 My <Text style={{fontWeight: '700'}}>Income</Text>
             </Text>
 
