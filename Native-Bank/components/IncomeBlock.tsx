92489be53f9b034d0278cd2522e68f5914f6879f
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-18 14:18:05 +0100

    Add spending block

--- Native-Bank/components/IncomeBlock.tsx
+++ Native-Bank/components/IncomeBlock.tsx
@@ -2,18 +2,28 @@ import { FlatList, ListRenderItem, StyleSheet, Text, TouchableOpacity, View } fr
 import React from "react";
 import Colors from "@/constants/Colors";
 import { IncomeType } from "@/types";
-import { DollarIcon } from "@/constants/icons";
+import { Image } from 'react-native';
+import { DollarIcon, WalletAddMoneyIcon } from "@/constants/icons";
 import { Feather } from "@expo/vector-icons";
-import amount from "../data/income.json";
+
+const WalletCardIcon = require("@/assets/icons/ewallet.png");
 
 const IncomeBlock = ({incomeList} : {incomeList: IncomeType[]}) => {
     const renderItem:ListRenderItem<IncomeType> = ({item, index}) => {
+        let icon = <DollarIcon width={22} height={22} color={Colors.white} />;
+        if (item.name == 'Freelancing') {
+            icon = <Image source={WalletCardIcon} style={{width: 22, height: 22, color: Colors.white}} />;
+        } else if (item.name == 'Interest') {
+            icon = <WalletAddMoneyIcon width={22} height={22} color={Colors.white} />;
+        }
+
         let [integerPart, decimalPart] = item.amount.split('.');
+        
         return(
             <View style={{backgroundColor: Colors.grey, padding: 20, borderRadius: 20, marginRight: 15, width: 150, gap: 10,}}>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
                 <View style= {{borderColor: '#666', borderWidth: 1, borderRadius: 50, padding: 5, alignSelf: 'flex-start',}}>
-                <DollarIcon width={22} height={22} color={Colors.white} />
+                {icon}
                 </View>
 
                 <TouchableOpacity onPress={() => {}}>
