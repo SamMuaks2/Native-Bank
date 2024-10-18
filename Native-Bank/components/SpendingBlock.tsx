d0fee3d514036d237335593b3a6f29c4072bd017
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-18 20:36:14 +0100

    Finish SpendingBlock

--- Native-Bank/components/SpendingBlock.tsx
+++ Native-Bank/components/SpendingBlock.tsx
@@ -16,81 +16,39 @@ const NetflixIcon = require("@/assets/icons/netflix.png");
 
 const SpendingBlock = ({ spendingList }: { spendingList: SpendingType[] }) => {
   return (
-    <View style={{ marginVertical: 20, alignItems: "flex-start" }}>
-      <Text style={{ color: Colors.white, fontSize: 16 }}>
-        July <Text style={{ fontWeight: "700" }}>Spending</Text>
+    <View style={styles.spendingBlock}>
+      <Text style={styles.mainText}>
+        July <Text style={styles.spending}>Spending</Text>
       </Text>
 
       {spendingList.map((item) => {
-        let icon = <DollarIcon width={22} height={22} color={Colors.white} />;
+        let icon = <DollarIcon style={styles.spendingIcon} />;
 
         if (item.name == "AirBnB Rent") {
-          icon = <AirbnbIcon width={22} height={22} color={Colors.white} />;
+          icon = <AirbnbIcon style={styles.spendingIcon} />;
         } else if (item.name == "Netflix") {
-          icon = (
-            <Image
-              source={NetflixIcon}
-              style={{ width: 22, height: 22, color: Colors.white }}
-            />
-          );
+          icon = <Image source={NetflixIcon} style={styles.spendingIcon} />;
         } else if (item.name == "Spotify") {
-          icon = <SpotifyIcon width={22} height={22} color={Colors.white} />;
+          icon = <SpotifyIcon style={styles.spendingIcon} />;
         } else if (item.name == "Amazon") {
-          icon = <AmazonIcon width={22} height={22} color={Colors.white} />;
+          icon = <AmazonIcon style={styles.spendingIcon} />;
         } else if (item.name == "Figma") {
-          icon = <FigmaIcon width={22} height={22} color={Colors.white} />;
+          icon = <FigmaIcon style={styles.spendingIcon} />;
         } else if (item.name == "Online Shopping") {
-          icon = (
-            <ShopingCartIcon width={22} height={22} color={Colors.white} />
-          );
+          icon = <ShopingCartIcon style={styles.spendingIcon} />;
         }
 
         return (
-          <View
-            key={item.id}
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginVertical: 10,
-            }}
-          >
-            <View
-              style={{
-                backgroundColor: Colors.grey,
-                padding: 15,
-                borderRadius: 50,
-                marginRight: 10,
-              }}
-            >
-              {icon}
-            </View>
+          <View key={item.id} style={styles.spendingWrapper}>
+            <View style={styles.iconWrapper}>{icon}</View>
 
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
+            <View style={styles.spendingSubWrapper}>
               <View style={{ gap: 5 }}>
-                <Text
-                  style={{
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: "600",
-                  }}
-                >
-                  {item.name}
-                </Text>
-                <Text style={{ color: Colors.white }}>{item.date}</Text>
+                <Text style={styles.spendingItemName}>{item.name}</Text>
+                <Text style={styles.spendingItemDate}>{item.date}</Text>
               </View>
 
-              <Text
-                style={{ color: Colors.white, fontSize: 16, fontWeight: "600" }}
-              >
-                ${item.amount}
-              </Text>
+              <Text style={styles.spendingItemAmount}>${item.amount}</Text>
             </View>
           </View>
         );
@@ -101,4 +59,50 @@ const SpendingBlock = ({ spendingList }: { spendingList: SpendingType[] }) => {
 
 export default SpendingBlock;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  spendingBlock: {
+    marginVertical: 20,
+    alignItems: "flex-start",
+  },
+  mainText: {
+    color: Colors.white,
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  spending: {
+    fontWeight: "700",
+  },
+  spendingIcon: {
+    width: 22,
+    height: 22,
+    color: Colors.white,
+  },
+  spendingWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  iconWrapper: {
+    backgroundColor: Colors.grey,
+    padding: 15,
+    borderRadius: 50,
+    marginRight: 10,
+  },
+  spendingSubWrapper: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  spendingItemName: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  spendingItemDate: { color: Colors.white },
+  spendingItemAmount: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
